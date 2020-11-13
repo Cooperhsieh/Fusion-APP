b4bf41a5f04b45b417ca6e0a157edac2f5ddcd10
Cooperhsieh <cooper79216@gmail.com>
2020-11-13 14:17:02 +0800

    Initial Commit

---
+++ Fusion APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fusion APP
+//
+//  Created by Cooper on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
